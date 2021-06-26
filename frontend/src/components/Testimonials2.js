de7942bd5b08eec789a413b5c62aa91a4ff6d4af
AvinashMahanthi <avinashfab5678@gmail.com>
2021-06-26 09:39:44 +0530

    feat(frontend): Updated content

--- frontend/src/components/Testimonials2.js
+++ frontend/src/components/Testimonials2.js
@@ -1,148 +1,156 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 import {
-    Box,
-    Flex,
-    Heading,
-    Text,
-    Stack,
-    Container,
-    Avatar,
-    useColorModeValue,
-} from '@chakra-ui/react';
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  Container,
+  Center,
+  Avatar,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const Testimonial = ({ children }) => {
-    return <Box>{children}</Box>;
+  return (
+    <Center>
+      <Box>{children}</Box>
+    </Center>
+  );
 };
 
 const TestimonialContent = ({ children }) => {
-    return (
-        <Stack
-            bg={useColorModeValue('white', 'gray.800')}
-            boxShadow={'lg'}
-            p={8}
-            rounded={'xl'}
-            align={'center'}
-            pos={'relative'}
-            _after={{
-                content: `""`,
-                w: 0,
-                h: 0,
-                borderLeft: 'solid transparent',
-                borderLeftWidth: 16,
-                borderRight: 'solid transparent',
-                borderRightWidth: 16,
-                borderTop: 'solid',
-                borderTopWidth: 16,
-                borderTopColor: useColorModeValue('white', 'gray.800'),
-                pos: 'absolute',
-                bottom: '-16px',
-                left: '50%',
-                transform: 'translateX(-50%)',
-            }}>
-            {children}
-        </Stack>
-    );
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      boxShadow={"lg"}
+      w={[300, 400]}
+      p={8}
+      rounded={"xl"}
+      align={"center"}
+      pos={"relative"}
+      _after={{
+        content: `""`,
+        w: 0,
+        h: 0,
+        borderLeft: "solid transparent",
+        borderLeftWidth: 16,
+        borderRight: "solid transparent",
+        borderRightWidth: 16,
+        borderTop: "solid",
+        borderTopWidth: 16,
+        borderTopColor: useColorModeValue("white", "gray.800"),
+        pos: "absolute",
+        bottom: "-16px",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
+      {children}
+    </Stack>
+  );
 };
 
 const TestimonialHeading = ({ children }) => {
-    return (
-        <Heading as={'h3'} fontSize={'xl'}>
-            {children}
-        </Heading>
-    );
+  return (
+    <Heading as={"h3"} fontSize={"xl"}>
+      {children}
+    </Heading>
+  );
 };
 
 const TestimonialText = ({ children }) => {
-    return (
-        <Text
-            textAlign={'center'}
-            color={useColorModeValue('gray.600', 'gray.400')}
-            fontSize={'sm'}>
-            {children}
-        </Text>
-    );
+  return (
+    <Text
+      textAlign={"center"}
+      color={useColorModeValue("gray.600", "gray.400")}
+      fontSize={"sm"}
+    >
+      {children}
+    </Text>
+  );
 };
 
-const TestimonialAvatar = ({
-    src,
-    name,
-    title,
-}) => {
-    return (
-        <Flex align={'center'} mt={8} direction={'column'}>
-            <Avatar src={src} alt={name} mb={2} />
-            <Stack spacing={-1} align={'center'}>
-                <Text fontWeight={600}>{name}</Text>
-                <Text fontSize={'sm'} color={useColorModeValue('gray.600', 'gray.400')}>
-                    {title}
-                </Text>
-            </Stack>
-        </Flex>
-    );
+const TestimonialAvatar = ({ src, name, title }) => {
+  return (
+    <Flex align={"center"} mt={8} direction={"column"}>
+      <Avatar src={src} alt={name} mb={2} />
+      <Stack spacing={-1} align={"center"}>
+        <Text fontWeight={600}>{name}</Text>
+        <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
+          {title}
+        </Text>
+      </Stack>
+    </Flex>
+  );
 };
 
 export default function WithSpeechBubbles() {
-    return (
-        <Box bg={useColorModeValue('gray.100', 'gray.700')}>
-            <Container maxW={'7xl'} py={16} as={Stack} spacing={12}>
-                <Stack spacing={0} align={'center'}>
-                    <Heading>Our Clients Speak</Heading>
-                    <Text>We have been working with clients around the world</Text>
-                </Stack>
-                <Stack
-                    direction={{ base: 'column', md: 'row' }}
-                    spacing={{ base: 10, md: 4, lg: 10 }}>
-                    <Testimonial>
-                        <TestimonialContent>
-                            <TestimonialHeading>Efficient Collaborating</TestimonialHeading>
-                            <TestimonialText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
-                                neque sed imperdiet nibh lectus feugiat nunc sem.
-                            </TestimonialText>
-                        </TestimonialContent>
-                        <TestimonialAvatar
-                            src={
-                                'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80'
-                            }
-                            name={'Jane Cooper'}
-                            title={'CEO at ABC Corporation'}
-                        />
-                    </Testimonial>
-                    <Testimonial>
-                        <TestimonialContent>
-                            <TestimonialHeading>Intuitive Design</TestimonialHeading>
-                            <TestimonialText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
-                                neque sed imperdiet nibh lectus feugiat nunc sem.
-                            </TestimonialText>
-                        </TestimonialContent>
-                        <TestimonialAvatar
-                            src={
-                                'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80'
-                            }
-                            name={'Jane Cooper'}
-                            title={'CEO at ABC Corporation'}
-                        />
-                    </Testimonial>
-                    <Testimonial>
-                        <TestimonialContent>
-                            <TestimonialHeading>Mindblowing Service</TestimonialHeading>
-                            <TestimonialText>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor
-                                neque sed imperdiet nibh lectus feugiat nunc sem.
-                            </TestimonialText>
-                        </TestimonialContent>
-                        <TestimonialAvatar
-                            src={
-                                'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80'
-                            }
-                            name={'Jane Cooper'}
-                            title={'CEO at ABC Corporation'}
-                        />
-                    </Testimonial>
-                </Stack>
-            </Container>
-        </Box>
-    );
+  return (
+    <Box bg={useColorModeValue("gray.100", "gray.700")}>
+      <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
+        <Stack spacing={0} align={"center"}>
+          <Heading>Our Clients Speak</Heading>
+          <Text>We have been working with clients, lets hear some reviews</Text>
+        </Stack>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          spacing={{ base: 10, md: 4, lg: 10 }}
+        >
+          <Testimonial wi>
+            <TestimonialContent>
+              <TestimonialHeading>Efficient Collaborating</TestimonialHeading>
+              <TestimonialText>
+                Very useful and handy website. It proved to be very handy when I
+                was travelling. A video consultation with the doctor and
+                following his advice got me back on the health track in no time!
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+              }
+              name={"Jane Cooper"}
+              title={"CEO at ABC Corporation"}
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Intuitive Design</TestimonialHeading>
+              <TestimonialText>
+                The best and easiest way to get consultation on phone/chat or
+                video calls, this is very useful especially for the employees
+                who have busy work schedule. it saves money as well as our
+                precious time.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1624484631620-9e53e4aed980?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8dG93SlpGc2twR2d8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              }
+              name={"Juliet"}
+              title={"Senior Accountant"}
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>Mindblowing Service</TestimonialHeading>
+              <TestimonialText>
+                No need to rush to the hospital every now and then Or we can say
+                that our health is in our pocket now. Great idea! |
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1623140922112-9cfc0e71b8fc?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU5fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              }
+              name={"Jhonathon"}
+              title={"Bank manager"}
+            />
+          </Testimonial>
+        </Stack>
+      </Container>
+    </Box>
+  );
 }
